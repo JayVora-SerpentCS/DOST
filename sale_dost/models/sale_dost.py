756d95065be67ff3a73e8aa40316b183a3bfc873
Jagdish Panchal <jagdish.panchal@serpentcs.com>
2019-06-25 12:04:49 +0530

    Migrated module in v12

--- sale_dost/models/sale_dost.py
+++ sale_dost/models/sale_dost.py
@@ -1,20 +1,35 @@
 # -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
-from openerp import fields, models,api
+from odoo import fields, models, api
+import base64
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+    
+    @api.multi
+    def picking_state(self):
+        shipped = False
+        for rec in self:
+            if rec.picking_ids:
+                picking_list = self.env['stock.picking'].search([
+                                            ('id', 'in', rec.picking_ids.ids),
+                                            ('state', '!=', 'done')])
+                if not picking_list:
+                    shipped = True
+            rec.shipped = shipped
 
     latitude = fields.Char('Latitude', copy=False)
-    longitude = fields.Char('Longitude', copy=False)
-    cust_sign = fields.Binary('Customer Sign', copy=False)
+    longitude = fields.Char('Longitude',  copy=False)
+    cust_sign = fields.Binary('Customer Sign',  copy=False)
     cust_sign_name = fields.Char('Customer Sign Name', copy=False)
-
+    shipped = fields.Boolean(compute='picking_state', string='Shipped')
+    
+    
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    prod_image = fields.Binary('Product Image', copy=False)
-    prod_img_name = fields.Char('Product Image Name', copy=False)
+    prod_image = fields.Binary('Product Image',  copy=False)
+    prod_img_name = fields.Char(' Received Product Image Name',  copy=False)
